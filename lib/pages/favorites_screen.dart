977c934f32c8d5f60bbd57d3c02f3e65430b94e0
MackayChandler1 <55098733+MackayChandler@users.noreply.github.com>
2021-11-26 11:08:17 -0700

    Update favorites_screen.dart

--- lib/pages/favorites_screen.dart
+++ lib/pages/favorites_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:restaurant_shuffle/models/restaurant.dart';
@@ -11,25 +12,6 @@ class FavoritesScreen extends StatefulWidget {
 }
 
 class _FavoritesScreen extends State<FavoritesScreen> {
-  List _favoriteList = [];
-
-  Future<void> getData() async {
-    List restaurantList = [];
-    final String response =
-        await rootBundle.loadString('assets/example_restaurants.json');
-    var list = json
-        .decode(response)['restaurants']
-        .map((data) => Restaurant.fromJson(data))
-        .toList();
-    for (Restaurant res in list) {
-      debugPrint(res.name);
-      restaurantList.add(res);
-    }
-    setState(() {
-      _favoriteList = restaurantList;
-    });
-  }
-
   @override
   void initState() {
     super.initState();
@@ -37,23 +19,39 @@ class _FavoritesScreen extends State<FavoritesScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final Stream<QuerySnapshot> _favoritesStream = FirebaseFirestore.instance
+        .collection('restaurants')
+        .where('favorite', isEqualTo: true)
+        .snapshots();
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Restaurant Shuffle - Favorites'),
-      ),
-      body: ListView.builder(itemBuilder: (context, index) {
-        return Container(
-          height: 50,
-          child: Row(
-            children: [
-              Text('${_favoriteList[index].name}'),
-              Container(),
-              IconButton(onPressed: star, icon: Icon(Icons.star_outline))
-            ],
-          ),
-        );
-      }),
-    );
+        appBar: AppBar(
+          title: const Text('Favorites'),
+        ),
+        body: StreamBuilder<QuerySnapshot>(
+          stream: _favoritesStream,
+          builder:
+              ((BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (snapshot.hasError) {
+              return Center(child: Text('Something went wrong'));
+            }
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: Text('Loading...'));
+            }
+            if (!snapshot.hasData) {
+              return Center(child: Text('No Favorites'));
+            }
+            return ListView(
+              children: snapshot.data!.docs.map((DocumentSnapshot document) {
+                Map<String, dynamic> data =
+                    document.data()! as Map<String, dynamic>;
+                return ListTile(
+                  title: Text(data['name']),
+                );
+              }).toList(),
+            );
+          }),
+        ));
   }
 
   void star() {
