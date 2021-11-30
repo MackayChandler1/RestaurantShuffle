81cba6d8bcf9a6869cb7b3adeeeb89fd926a29e1
sderbidge <55034668+sderbidge@users.noreply.github.com>
2021-11-29 21:26:01 -0700

    Added filter category drop down menu.

--- lib/pages/filter_screen.dart
+++ lib/pages/filter_screen.dart
@@ -6,12 +6,49 @@ class FilterScreen extends StatefulWidget {
   _FilterScreenState createState() => _FilterScreenState();
 }
 
+String dropdownValue = "burger";
+
+var items = ["burger", "mexican"];
+
 class _FilterScreenState extends State<FilterScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Restaurant Shuffle - Filter'),
+      ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+              width: 300.0,
+              child: DropdownButton<String>(
+                value: dropdownValue,
+                elevation: 16,
+                style: const TextStyle(color: Colors.blue),
+                underline: Container(
+                  height: 2,
+                  color: Colors.blueAccent,
+                ),
+                onChanged: (String? newValue) {
+                  setState(() {
+                    dropdownValue = newValue!;
+                  });
+                },
+                items: items.map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(
+                      value,
+                      textAlign: TextAlign.center,
+                      style: new TextStyle(
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  );
+                }).toList(),
+              ))
+        ],
       ),
     );
   }
